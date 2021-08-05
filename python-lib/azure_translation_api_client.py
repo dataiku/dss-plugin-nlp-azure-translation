f1100b5b58ecfc9f64c0105fbcfa6de50421062a
Niklas Muennighoff <niklasmuennighoff@Niklass-MBP.home>
2021-08-05 18:12:29 +0200

    Set global definitions

--- python-lib/azure_translation_api_client.py
+++ python-lib/azure_translation_api_client.py
@@ -14,6 +14,8 @@ import requests
 
 
 API_EXCEPTIONS = (requests.HTTPError,)
+AZURE_ENDPOINT = "https://api.cognitive.microsofttranslator.com/translate"
+AZURE_VERSION = "3.0"
 
 
 # ==============================================================================
@@ -34,8 +36,6 @@ class AzureTranslatorClient:
     def __init__(self, api_key, location) -> None:
         self.api_key = api_key
         self.location = location
-
-        self.endpoint = "https://api.cognitive.microsofttranslator.com/translate"
 
     def translate(
         self,
@@ -58,19 +58,14 @@ class AzureTranslatorClient:
             HTTPError: An error occured accessing the API.
         """
         response = requests.post(
-            url=self.endpoint,
+            url=AZURE_ENDPOINT,
             headers={
                 "Ocp-Apim-Subscription-Key": self.api_key,
                 "Ocp-Apim-Subscription-Region": self.location,
                 "Content-type": "application/json",
             },
-            params={"api-version": "3.0", "from": source_language, "to": target_language},
-            data={
-                "source_lang": source_language,
-                "target_lang": target_language,
-                "auth_key": self.api_key,
-                "text": text,
-            },
+            params={"api-version": AZURE_VERSION, "from": source_language, "to": target_language},
+            json=[{"text": text}],
         )
         if response.status_code == requests.codes.ok:
             # Returns text from the response object which is a json string, so no need to dump it into json anymore
